63f73affb832b3f7ce1a95de73ef3deecf786c34
venugopalsugumar <venugopalsugumar1467@gmail.com>
2026-03-12 23:04:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+const bookingForm = document.querySelector(".booking-form");
+
+if (bookingForm) {
+
+bookingForm.addEventListener("submit", function (e) {
+
+e.preventDefault();
+
+const name = bookingForm.querySelector('input[type="text"]').value;
+const phone = bookingForm.querySelectorAll('input[type="text"]')[1].value;
+const location = bookingForm.querySelectorAll('input[type="text"]')[2].value;
+const service = bookingForm.querySelector("select").value;
+
+if(name === "" || phone === "" || location === ""){
+
+alert("Please fill all required fields");
+
+return;
+
+}
+
+alert(
+"Booking Confirmed!\n\n" +
+"Name: " + name +
+"\nService: " + service +
+"\nLocation: " + location
+);
+
+bookingForm.reset();
+
+});
+
+}
+
+});
+document.querySelectorAll("button[data-link]").forEach(button => {
+button.addEventListener("click", () => {
+window.location.href = button.getAttribute("data-link");
+});
+});
